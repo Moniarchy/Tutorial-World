a362eb9d3dd49090c8b00a3b3b0e129c5f6bcadf
jrob8577 <roberts.john@gmail.com>
2016-07-25 16:14:51 -0700

    Created static home page document

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var app = express();
+var logger = require('morgan');
 var exphbs = require('express3-handlebars');
+
+app.use(logger('dev'));
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res){
-	var luckyNumber = Math.round( Math.random() * 10);
-	res.render('index', {
-		luckyNumber: luckyNumber
-	});
+app.get('/', function( request, response ){
+  var luckyNumber = Math.round( Math.random() * 10);
+  response.render('index', {
+    luckyNumber: luckyNumber
+  });
 });
 
-app.get('/about', function(req, res){
-	res.render('about');
+app.get('/about', function( request, response ){
+  response.render('about');
 });
 
 app.use('/public', express.static('public'));
