67a2de56adc2d3b39ecf8573c192a730cda8b5c8
Rachel Ralston <rachelralston@gmail.com>
2016-07-25 14:27:40 -0700

    basic index and server stuff set up

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var app = express();
 var exphbs = require('express3-handlebars');
-app.engine('handlebars', exphbs({defaultlayout: 'main'}));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res){
-	res.render('index');
+	var luckyNumber = Math.round( Math.random() * 10);
+	res.render('index', {
+		luckyNumber: luckyNumber
+	});
+});
+
+app.get('/about', function(req, res){
+	res.render('about');
 });
 
 app.use('/public', express.static('public'));
 
-var port = Number(process.env.PORT || 5000);
+var port = Number( process.env.PORT || 5000 );
 app.listen(port);
